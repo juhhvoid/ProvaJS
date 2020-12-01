e6dc8c714f86df5d4e72a0104fd4b2796484f09f
Leonardo de Lima Junior <juhhvoid@gmail.com>
2020-12-01 13:44:34 -0300

    att2

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const venc = document.getElementById('venc')
 const tip = document.getElementById('tip')
 const tbody = document.getElementById('tbody')
 const res = document.getElementById('res')
+
+
+
 let saldo = 0
 Number(saldo.value)
 res.innerHTML = `Saldo: ${saldo}`
 
+
 const saveData = () => {
+
     let linha = document.createElement('tr')
     let col1 = document.createElement('td')
     col1.text = desc.value
@@ -37,13 +42,16 @@ const saveData = () => {
     col4.style.border = "1px solid black"
 
     Number(val.value)
-    if (tip.value == 'Receita') {
-        saldo += val.value
-    }else if (tip.value == 'Despesa') {
-        saldo -= val.value
+    if (tip.value == 'receita') {
+        saldo += Number(val.value)
+    }else if (tip.value == 'despesa') {
+        saldo -= Number(val.value)
     }
     res.innerHTML = `Saldo: ${saldo}`
+    
 }
+
+
 
 
 
